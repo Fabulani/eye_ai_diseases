88790774f5803e2ee05a53f2e0e088b24715dd9c
out0 <cristianoo@gmail.com>
2022-03-09 08:12:13 -0300

    output messages, code cleanup, preparing to train model

--- deep-learning/eye_disease_classification.py
+++ deep-learning/eye_disease_classification.py
@@ -1,25 +1,6 @@
-# %%
-from io import StringIO
-from xmlrpc.client import Boolean
-import torch
+#! /usr/bin/python3
 import torch.nn as nn
-import torch.nn.functional as F
-from torchvision.datasets import VisionDataset
-from torch.utils.data import Dataset, DataLoader
-from enum import Enum, IntEnum, unique, auto
-from typing import Tuple, List, Union, Any, Optional, Callable
-import pandas as pd
-import numpy as np
-import functools
-import skimage.io as io
-from skimage import data, color
-from skimage.transform import rescale, resize, downscale_local_mean
-from skimage.filters import threshold_mean, sobel
-from skimage.color import rgb2gray
-from skimage import feature
-import os.path as ospath
-from PIL import Image
-import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 
 from eye_classifier import *
 from eye_dataset import *
@@ -30,14 +11,17 @@ image_dir = f"{base_dir}/preprocessed_images"
 csv_file = f'{base_dir}/ODIR-5K/data.csv'
 
 #ds = read_images(base_dir, image_path=image_dir, data_info_csv_file=csv_file, limit_input_count=100)
+print ('reading input dataset')
 ds = read_images(base_dir, image_path=image_dir, data_info_csv_file=csv_file)
-
-print(ds.targets[0])
 train_loader = DataLoader(ds, batch_size=4, shuffle=True)
 
+print ('building model')
 nn = EyeClassifier(image_shape=[512,512,3], num_classes=len(ds.classes))
 print(nn)
 
+print ('training model')
 nn.train(ds)
+
+print ('testing model')
 nn.test(ds)
 
